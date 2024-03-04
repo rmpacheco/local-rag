d626767b4d398ac17480749da82098117d612d6a
Román M. Pacheco <rmpacheco@gmail.com>
2024-03-03 19:53:50 -0600

    added requirements.txt file, and tweaked prompt

--- rag.py
+++ rag.py
@@ -16,24 +16,24 @@ class ChatPDF:
 
     def __init__(self):
         self.model = ChatOllama(model="mistral")
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
-        self.prompt = PromptTemplate.from_template(
-            """
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024,
+                                                            chunk_overlap=100)
+        self.prompt = PromptTemplate.from_template("""
             <s> [INST] You are an assistant for question-answering tasks. Use the following pieces of retrieved context 
-            to answer the question. If you don't know the answer, just say that you don't know. Use three sentences
-             maximum and keep the answer concise. [/INST] </s> 
+            to answer the question. If you don't know the answer, just say that you don't know. Be as accurate and true to the 
+            retrieved context as possible. [/INST] </s> 
             [INST] Question: {question} 
             Context: {context} 
             Answer: [/INST]
-            """
-        )
+            """)
 
     def ingest(self, pdf_file_path: str):
         docs = PyPDFLoader(file_path=pdf_file_path).load()
         chunks = self.text_splitter.split_documents(docs)
         chunks = filter_complex_metadata(chunks)
 
-        vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        vector_store = Chroma.from_documents(documents=chunks,
+                                             embedding=FastEmbedEmbeddings())
         self.retriever = vector_store.as_retriever(
             search_type="similarity_score_threshold",
             search_kwargs={
@@ -42,7 +42,10 @@ class ChatPDF:
             },
         )
 
-        self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+        self.chain = ({
+            "context": self.retriever,
+            "question": RunnablePassthrough()
+        }
                       | self.prompt
                       | self.model
                       | StrOutputParser())
